319d53ed37d450787599cc79196fd48102d0c16b
Maryam597 <maryam.dri@hotmail.fr>
2023-07-03 09:54:00 +0200

    js now

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const button1 = document.querySelector('#button1');
+const button1 = document.querySelector('button1');
 const description = document.querySelector('#description');
 
-
-button1.addEventListener('click')
+button1.addEventListener('click', ()=>{   //fonction fléchée 
+    if(description.style.display == 'block'){
+    description.style.display = 'none';
+}
+else {
+    description.style.display = 'block';
+}
+});
