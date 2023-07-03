7305016de5870906de22c9644770466addb85b4f
Maryam597 <maryam.dri@hotmail.fr>
2023-07-03 14:01:02 +0200

    nooooooooo

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// const button1 = document.querySelector('button1');
-// const description = document.querySelector('#description');
-
-// button1.addEventListener('click', ()=>{   //fonction fléchée 
-//     if(description.style.display == 'block'){
-//     description.style.display = 'none';
-// }
-// else {
-//     description.style.display = 'block';
-// }
-// });
-
-
 const button = document.getElementById('button');
 const description = document.getElementsByClassName('description');
 const bw1 = document.getElementById('bw1');
@@ -22,10 +9,12 @@ console.log();
 
 button.addEventListener('click', ()=> {
     bw1.classList.toggle('active');
-});
 
+
+});
 
 bw.addEventListener('click', ()=> {
     bw1.classList.toggle('active');
 
-});
+})
+
